bc644f0515f9a063a6bc9fbaaf2f50dfb8589b79
Ivan D. Tarnyagin <ivan.tarnyagin@gmail.com>
2021-04-02 12:31:17 +0200

    Just made a scaffolding for my vue-router app😀😁😎😎

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/projects">Projects</router-link> |
+    <router-link to="/services">Services</router-link> 
   </div>
   <router-view />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Projects from "../views/Projects.vue";
+import Services from "../views/Services.vue";
+import About from "../views/About.vue";
 
 const routes = [
   {
@@ -8,13 +11,19 @@ const routes = [
     component: Home,
   },
   {
+    path: "/projects",
+    name: "Projects",
+    component: Projects,
+  },
+  {
+    path: "/services",
+    name: "Services",
+    component: Services,
+  },
+  {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: About,
   },
 ];
 
